261461e6ab78184e3d11c7b69a490749b0ac1384
Anton Zimin <56966172+zimkaa@users.noreply.github.com>
2024-11-04 22:46:25 +0500

    fix
    Signed-off-by: Anton Zimin <56966172+zimkaa@users.noreply.github.com>

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -5,100 +5,117 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";  
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
-  let
-    configuration = { pkgs, config, ... }: {
+  outputs = {
+    self,
+    nix-darwin,
+    nixpkgs,
+    nix-homebrew,
+  } @ inputs: let
+    configuration = {
+      pkgs,
+      config,
+      ...
+    }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
 
+      nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
       nixpkgs.config.allowUnfree = true;
-	
-      environment.systemPackages =
-        [ 
-	  pkgs.vim
-	  pkgs.neovim
-	  pkgs.tmux
-	  pkgs.kitty
-	  pkgs.mkalias
-	  pkgs.obsidian
-	  pkgs.stow
-	  pkgs.btop
-	  pkgs.htop
-	  pkgs.pyenv
-	  pkgs.fzf
-	  pkgs.zoxide
-	  pkgs.yazi
-	  pkgs.eza
-	  pkgs.poetry
-	  pkgs.oh-my-posh
-	  pkgs.duf
-	  pkgs.fd
-	  pkgs.bat
-	  pkgs.lazydocker
-	  pkgs.dust
-	  pkgs.lazygit
-	  pkgs.ripgrep
-	  pkgs.discord
-	  pkgs.tldr
-	  pkgs.devbox
-	  pkgs.go-task
-        ];
-      
-      fonts.packages = [
-	(pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })	
+
+      environment.systemPackages = [
+        pkgs.vim
+        pkgs.neovim
+        pkgs.tmux
+        pkgs.kitty
+        pkgs.mkalias
+        pkgs.obsidian
+        pkgs.stow
+        pkgs.btop
+        pkgs.htop
+        pkgs.pyenv
+        pkgs.fzf
+        pkgs.zoxide
+        pkgs.yazi
+        pkgs.eza
+        pkgs.poetry
+        pkgs.oh-my-posh
+        pkgs.duf
+        pkgs.fd
+        pkgs.bat
+        pkgs.lazydocker
+        pkgs.dust
+        pkgs.lazygit
+        pkgs.ripgrep
+        pkgs.discord
+        pkgs.tldr
+        pkgs.devbox
+        pkgs.go-task
+        pkgs.nixd
+        pkgs.alejandra
+        pkgs.cookiecutter
       ];
 
+      fonts.packages = [
+        (pkgs.nerdfonts.override {fonts = ["FiraCode"];})
+      ];
+
+      security.pam.enableSudoTouchIdAuth = true;
+
       system.defaults = {
-	dock.autohide = true;
-	#dock.persistent-apps = [
-	#  ""
-	#];
-	finder.FXPreferredViewStyle = "clmv";
-	loginwindow.GuestEnabled = false;
-	NSGlobalDomain.AppleICUForce24HourTime = true;
-	NSGlobalDomain.AppleInterfaceStyle = "Dark";
-	NSGlobalDomain.KeyRepeat = 2;
+        dock.autohide = true;
+        #dock.persistent-apps = [
+        #  ""
+        #];
+        dock.mru-spaces = true;
+        finder.AppleShowAllExtensions = true;
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.GuestEnabled = false;
+        NSGlobalDomain.AppleICUForce24HourTime = true;
+        NSGlobalDomain.AppleInterfaceStyle = "Dark";
+        NSGlobalDomain.KeyRepeat = 2;
       };
 
       homebrew = {
-	enable = true;
-	brews = [
-	  "mas"
-	  "syncthing"
-	];
-	casks = [
-	  "firefox"
-	  "iina"
-	  "the-unarchiver"
-	  "docker"
-	  "maccy"
-	  "stats"
-	  "rectangle"
-	  "devtoys"
-	  "hiddenbar"
-	  "cheatsheet"
-	  "bitwarden"
-	  "appcleaner"
-	  "pycharm"
-	  "caffeine"
-	  "keycastr"
-	  "thunderbird"
-	  "obs"
-	  "pritunl"
-	  "dbeaver-community"
-	];
-	masApps = {
-	  "hand mirror" = 1502839586;
-	  "outline-secure-internet-access" = 1356178125;
-	  "Telegram Lite" = 946399090;
-	  "Slack for Desktop" = 803453959;
-	};
-	onActivation.cleanup = "zap";
-	onActivation.autoUpdate = true;
-	onActivation.upgrade = true;
+        enable = true;
+        brews = [
+          "mas"
+          "syncthing"
+        ];
+        casks = [
+          "firefox"
+          "iina"
+          "the-unarchiver"
+          "docker"
+          "maccy"
+          "stats"
+          "rectangle"
+          "devtoys"
+          "hiddenbar"
+          "cheatsheet"
+          "bitwarden"
+          "appcleaner"
+          "pycharm"
+          "caffeine"
+          "keycastr"
+          "thunderbird"
+          "obs"
+          "pritunl"
+          "dbeaver-community"
+          "visual-studio-code"
+        ];
+        masApps = {
+          "hand mirror" = 1502839586;
+          "outline-secure-internet-access" = 1356178125;
+          "Telegram Lite" = 946399090;
+          "Slack for Desktop" = 803453959;
+        };
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
       };
 
       system.activationScripts.applications.text = let
@@ -109,17 +126,17 @@
         };
       in
         pkgs.lib.mkForce ''
-        # Set up applications.
-        echo "setting up /Applications..." >&2
-        rm -rf /Applications/Nix\ Apps
-        mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read src; do
-          app_name=$(basename "$src")
-          echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-        done
-            '';
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -129,7 +146,7 @@
       nix.settings.experimental-features = "nix-command flakes";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
+      programs.zsh.enable = true; # default shell on catalina
       # programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
@@ -142,25 +159,24 @@
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
-  in
-  {
+  in {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."macpro" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-	configuration
-	nix-homebrew.darwinModules.nix-homebrew
-	{
-	  nix-homebrew = {
-	    enable = true;
-	    # Apple Silicon Only
-	    enableRosetta = true;
-	    # User owning the Homebrew prefix
-	    user = "antonzimin";
-	    # if already installed homebrew
-	    # autoMigrate = true;
-	  };
-	} 
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            # Apple Silicon Only
+            enableRosetta = true;
+            # User owning the Homebrew prefix
+            user = "antonzimin";
+            # if already installed homebrew
+            # autoMigrate = true;
+          };
+        }
       ];
     };
 
