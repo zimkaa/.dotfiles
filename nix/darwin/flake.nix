799d7401e081db5dbd0fde01bbd84e785ed3d66f
Anton Zimin <56966172+zimkaa@users.noreply.github.com>
2025-02-13 20:09:18 +0500

    delete old config

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -148,8 +148,6 @@
           done
         '';
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
 
       # Necessary for using flakes on this system.
