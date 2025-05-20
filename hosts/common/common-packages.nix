3c196426683900433cff9b43eb36728bc705aa02
Anton Zimin <56966172+zimkaa@users.noreply.github.com>
2025-05-20 08:18:13 +0500

    added common package list
    Signed-off-by: Anton Zimin <56966172+zimkaa@users.noreply.github.com>

--- hosts/common/common-packages.nix
+++ hosts/common/common-packages.nix
@@ -1,49 +1,9 @@
 { inputs, pkgs, ... }:
-{
+let
+    packageNames = import ./packages.nix;
+in {
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    # # nixpkgs-unstable.legacyPackages.${pkgs.system}.beszel
-    # # nixpkgs-unstable.legacyPackages.${pkgs.system}.talosctl
-    # nixpkgs.legacyPackages.${pkgs.system}.talosctl
-
-    air  # for GO develop
-    alejandra
-    bat
-    btop
-    cookiecutter
-    devbox
-    duf
-    dust
-    eza
-    fd
-    fzf
-    go
-    go-task
-    htop
-    httpie
-    lazydocker
-    lazygit
-    neohtop
-    neovim
-    nixd
-    obsidian
-    oh-my-posh
-    poetry
-    pyenv
-    ripgrep
-    rustup
-    stow
-    templ  # LSP for GO develop
-    tldr
-    tmux
-    uv
-    vim
-    yazi
-    zoxide
-
-    # # requires nixpkgs.config.allowUnfree = true;
-    # vscode-extensions.ms-vscode-remote.remote-ssh
-  ];
+  environment.systemPackages = (map (name: pkgs.${name}) packageNames);
 }
